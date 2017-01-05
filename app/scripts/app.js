82d5e731aec5f37c6d1ddd661bf3784216c4a557
Austin Du <austin@flare-app.com>
2017-01-04 23:12:27 -0500

    added partial home page. yeoman angular. grunt. bower.

---
+++ app/scripts/app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name personalWebsiteStagingApp
+ * @description
+ * # personalWebsiteStagingApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('personalWebsiteStagingApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ngMaterial',
+    'ngAria',
+    'ngMessages'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
